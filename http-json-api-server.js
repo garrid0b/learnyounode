042d76ce73d37f94e4876875dbef6e724001a16a
Andres Garrido <andresrosmila@gmail.com>
2023-06-18 13:19:50 -0500

    tareas finalizadas

--- http-json-api-server.js
+++ http-json-api-server.js
@@ -1,12 +1,24 @@
 const http = require('http')
-const fs = require('fs')
-const port = Number(process.argv[2])
-const patch = process.argv[3]
+const url = require('url')
+const port = process.argv[2]
 
 const server =  http.createServer( (req,res) =>{
-  var texto =  fs.readFileSync(patch)
-  res.writeHead(200, { 'content-type': 'text/plain' })  
-  res.end(texto)    
+    var page = url.parse(req.url,true)      
+    var hour = new Date(page.query.iso)
+    const ParseTime =   {
+        "hour": hour.getHours(),
+        "minute": hour.getMinutes(),
+        "second": hour.getSeconds()
+      }
+    const unix = { 
+        "unixtime": hour.getTime()
+    }    
+    res.writeHead(200, { 'content-type': 'application/json' })  
+      if (page.pathname==='/api/parsetime'){
+        res.end(JSON.stringify(ParseTime))
+      }
+      if (page.pathname==='/api/unixtime'){
+        res.end(JSON.stringify(unix))
+      }  
 })
-
 server.listen(port)
